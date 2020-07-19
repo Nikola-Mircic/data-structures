30f3dfc71017e743d0ad2e28e6beb6c65efec085
unknown <nikola123mircic@gmail.com>
2020-07-19 18:05:23 +0200

    Update vector/vector.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include <cstdio>
 #include "vector/vector.cpp"
 
+struct man{
+    std::string name;
+    int age;
+};
+
+int printFunc1(man x){
+    return x.age;
+}
+
+std::string printFunc2(man x){
+    return x.name;
+}
+
 void printTextOnStart(){
     printf(" DATA STRUCUTRES EXAMPLES\n");
     printf("==============================================\n");
     printf(" For more informations please read \"README.md\"\n");
+    printf("\n");
 }
 
 int main()
 {
-    printTextOnStart();
+    int arr[5] = {1,2,3,4,5};
+    myVector<int> test(arr,arr+5);
+    test.add_elements(arr+3,arr+5);
+    test.print();
     return 0;
 }
--- vector/vector.cpp
+++ vector/vector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <algorithm>
 
 template <class TYPE> class myVector{
 private:
@@ -26,18 +27,16 @@ public:
     };
     //Creates vector with elements of an array in range [arr_start,arr_end)
     myVector(TYPE * arr_start,TYPE * arr_end){
-        if(main_arr != NULL)
-            delete main_arr;
-
         int n = arr_end-arr_start;
 
-        curr_size = n;
+        curr_size = 0;
         reserved_size = n;
 
         main_arr = (TYPE*) malloc(n*sizeof(TYPE));
 
-        for(int i=0;i<n;++i)
-            main_arr[i] = arr_start[i];
+        for(TYPE* i = arr_start; i!=arr_end; ++i){
+            main_arr[curr_size++] = *i;
+        }
     };
 
     void add_element(TYPE element){
@@ -46,28 +45,65 @@ public:
         main_arr[curr_size++] = element;
     }
 
+    void add_elements(TYPE * arr_start,TYPE * arr_end){
+        for(TYPE * i = arr_start;i!=arr_end;++i)
+            add_element(*i);
+    }
+
+    //Returns element at index 'idx';
     TYPE get(int idx){
         return main_arr[idx];
     }
 
+    //Returns index of first element with value equals x
+    int indexOf(TYPE x){
+        for(int i=0;i<curr_size;++i){
+            if(main_arr[i]==x)
+                return i;
+        }
+        return -1;
+    }
+
     //remove first element with value equals to x
     void rm_element(TYPE x){
+        int toMove = -1;
+        for(int i=0;i<curr_size;++i){
+            if(main_arr[i]==x && toMove = -1){
+                toMove = i;
+            }else{
+                main_arr[toMove++] = main_arr[i];
+            }
+        }
+        curr_size = toMove;
+    }
+
+    //remove all elements with value equals to x
+    void rm_elements(TYPE x){
         int toMove = 0;
         for(int i=0;i<curr_size;++i){
             if(main_arr[i]!=x){
                 main_arr[toMove++] = main_arr[i];
             }
         }
-        --curr_size;
+        curr_size = toMove;
     }
 
+    //returns number of elements in array
     int getSize(){
         return curr_size;
     }
 
+    //print elements of arr;
     void print(){
         for(int i=0;i<curr_size;++i)
             std::cout << main_arr[i] << " ";
+        std::cout << std::endl;
+    }
+
+    //print elements using passed function
+    template<typename PRINT_TYPE> void custom_print(PRINT_TYPE func(TYPE x)){
+        for(int i=0;i<curr_size;++i)
+            std::cout << func(main_arr[i]) << " ";
         std::cout << std::endl;
     }
 };
